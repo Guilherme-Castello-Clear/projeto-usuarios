b5f68d5faa8c5d7706a669b77583e5e649218b7b
Guilherme Augusto Castello Costa <guilherme.castello.clear@gmail.com>
2023-02-16 10:42:59 -0300

    Feat: Added user registration system

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
-fields.forEach(function(field, index){
+var user = {};
 
-    if(field.name == 'gender' && field.checked){
-        console.log("SIM", field);
-    }
-    else{
-        console.log("NÃO");
-    }
+function addLine(dataUser){
 
-    //console.log(index, field.name);
+    console.log(dataUser);
+    
+    document.getElementById("table-users").innerHTML = `<tr>
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  </tr>`
+}
 
-});
+console.log(user);
+
+document.getElementById("form-user-create").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    fields.forEach(function(field, index){
+
+        if(field.name == 'gender' && field.checked){
+            user[field.name] = field.value;
+        }
+        else{
+            user[field.name] = field.value;
+        }
+    });
+
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin);
+    addLine(objectUser);
+})
