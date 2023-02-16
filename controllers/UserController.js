57e7070b1b3426d51b8d8d10e0cdfba4fbdc8417
Guilherme Augusto Castello Costa <guilherme.castello.clear@gmail.com>
2023-02-16 12:25:18 -0300

    Fix: Fixed error on create a user without image

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,36 +14,48 @@ class UserController{
             event.preventDefault();
 
             let values = this.getValues();
-            this.getPhoto((content)=>{
 
-                values.photo = content;
-                this.addLine(values);
+            this.getPhoto().then(
+                (content)=>{
+                    values.photo = content;
+                    this.addLine(values);
 
-
+                },
+                (e)=>{
+                    console.error(e);
             });
-
+            
         });
 
     }
 
-    getPhoto(callback){
-        let fileReader = new FileReader();
-        let elements = [...this.formEl.elements].filter(item=>{
-            if(item.name === 'photo'){
-                return item;
+    getPhoto(){
+
+        return new Promise((resolve, reject)=>{
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item=>{
+                if(item.name === 'photo'){
+                    return item;
+                }
+            });
+    
+            let file = elements[0].files[0];
+    
+            fileReader.onload = () =>{
+                resolve(fileReader.result);
+    
+            }
+            fileReader.onerror = (e)=>{
+                reject(e);
+            }
+            if(file){
+                fileReader.readAsDataURL(file);
+            }
+            else{
+                resolve('dist/img/boxed-bg.jpg');
             }
         });
-
-        let file = elements[0].files[0];
-
-        fileReader.onload = () =>{
-
-
-            
-            callback(fileReader.result);
-
-        }
-        fileReader.readAsDataURL(file);
+        
     }
 
 
@@ -55,6 +67,13 @@ class UserController{
             if(field.name == 'gender' && field.checked){
                 user[field.name] = field.value;
             }
+            else if(field.name == "admin"){
+
+                user[field.name] = field.checked;
+
+            }
+
+
             else{
                 user[field.name] = field.value;
             }
@@ -74,17 +93,18 @@ class UserController{
     }
 
     addLine(dataUser){
-        
-        this.TableEl.innerHTML = `<tr>
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : "Não"}</td>
         <td>${dataUser.birth}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
-      </tr>`
+        `
+        this.TableEl.appendChild(tr);
     }
 }
