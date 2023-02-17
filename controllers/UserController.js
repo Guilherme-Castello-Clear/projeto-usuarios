296b63df5642eec2fd14aefa6d238fb9379ef770
Guilherme Augusto Castello Costa <guilherme.castello.clear@gmail.com>
2023-02-17 08:39:00 -0300

    Fix: Improved creation date system

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,11 +7,15 @@ class UserController{
 
     }
 
+
     onSubmit(){
        //Take the the click on Submit button
         this.formEl.addEventListener("submit", (event)=>{
             
             event.preventDefault();//Stop page refresh.
+
+            let btn = this.formEl.querySelector("[type=submit]")
+            btn.disabled = true;
 
             let values = this.getValues();
 
@@ -19,6 +23,9 @@ class UserController{
                 (content)=>{
                     values.photo = content;
                     this.addLine(values);
+                    this.formEl.reset();
+                    btn.disabled = false;
+                    
 
                 },
                 (e)=>{
@@ -99,7 +106,7 @@ class UserController{
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
         <td>${(dataUser.admin) ? 'Sim' : "Não"}</td>
-        <td>${dataUser.birth}</td>
+        <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
--- models/User.js
+++ models/User.js
@@ -2,15 +2,77 @@ class User{
 
     constructor(name, gender, birth, country, email, password, photo, admin){
         //Define User Object Attributes
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.country = country;
-        this.email = email;
-        this.password = password;
-        this.photo = photo;
-        this.admin = admin;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
 
     }
 
+    get register(){
+        return this._register;
+    }
+    get name(){
+        return this._name
+    }
+
+    get gender(){
+        return this._gender
+    }
+    get birth(){
+        return this._birth
+    }
+    get country(){
+        return this._country
+    }
+    get email(){
+        return this._email
+    }
+    get password(){
+        return this._password
+    }
+    get photo(){
+        return this._photo
+    }
+    get admin(){
+        return this._admin
+    }
+
+
+    //
+
+    set register(value){
+        return this._register;
+    }
+    set name(value){
+        return this._name
+    }
+
+    set gender(value){
+        return this._gender
+    }
+    set birth(value){
+        return this._birth
+    }
+    set country(value){
+        return this._country
+    }
+    set email(value){
+        return this._email
+    }
+    set password(value){
+        return this._password
+    }
+    set photo(value){
+        this._photo = value;
+    }
+    set admin(value){
+        return this._admin
+    }
+    
 }
